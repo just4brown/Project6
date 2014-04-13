3603dc5e5068d0198c7d8dc66904094a02926696
just4brown <justinbrown@cedarville.edu>
2014-04-13 18:28:39 -0400

    Imported G's files

---
+++ InstantMsg/src/imserver/BasicServer.java
@@ -0,0 +1,63 @@
+package imserver;
+
+import java.io.*;
+import java.net.*;
+
+public class BasicServer {
+
+  private int listenPort;
+  private int maxConnections;
+  protected int numConnections;
+
+  public BasicServer() {
+    this (4220, 0);
+  }
+  public BasicServer(int port, int numConnects) {
+    listenPort = port;
+    maxConnections = numConnects;
+    numConnections = 0;
+  }
+
+  public void start () {
+
+    try {
+      ServerSocket listenSocket = new ServerSocket (listenPort);
+
+      while (true) {
+        Socket newConnect;
+        if ( (maxConnections == 0) || (numConnections <= maxConnections) ) {
+          newConnect = listenSocket.accept();
+	  System.out.println("accepted "+newConnect.getInetAddress());
+	  System.out.flush();
+          numConnections++;
+          serviceConnection (newConnect);
+        }
+      }
+    }
+    catch (IOException ioe ){
+      System.out.println("Unable to listen on port "+listenPort);
+      ioe.printStackTrace();
+    }
+  }
+  protected void serviceConnection (Socket connection) throws IOException {
+
+    BufferedReader in = new BufferedReader
+           (new InputStreamReader(connection.getInputStream()));
+
+    PrintWriter out = new PrintWriter
+           (new PrintWriter(connection.getOutputStream(), true));
+
+    out.println("Hello from your server");
+    out.flush();
+
+    System.out.println("Closing connection");
+    System.out.flush();
+
+    connection.close();
+    numConnections--;
+  }
+  public static void main (String [] args) {
+    BasicServer myServer = new BasicServer();
+    myServer.start();
+  }
+}
---
+++ InstantMsg/src/imserver/ServerConnectionThread.java
@@ -0,0 +1,18 @@
+package imserver;
+
+import java.io.*;
+import java.net.*;
+
+public class ServerConnectionThread extends Thread {
+
+  private Socket mySocket;
+
+  public ServerConnectionThread(Runnable serverObject, Socket serverSocket) {
+    super (serverObject);
+    mySocket = serverSocket;
+  }
+
+  public Socket getSocket() {
+    return mySocket;
+  }
+}
