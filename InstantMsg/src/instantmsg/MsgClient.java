778c6969ba9331b0dee7da6cc58ec92c2c94be54
just4brown <justinbrown@cedarville.edu>
2014-04-13 20:55:56 -0400

    added msgClient

---
+++ InstantMsg/src/instantmsg/MsgClient.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package instantmsg;
+
+/**
+ *
+ * @author Brown, Justin
+ */
+public class MsgClient {
+
+}
